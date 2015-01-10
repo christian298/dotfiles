3f469117b22db9906ba00331cefcfe612850ef0b
Christian Sandvoß <christian.sandvoss@gmail.com>
2015-01-10 18:14:40 +0100

    added customizations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,12 +20,27 @@
     clojure-mode
     clojure-mode-extra-font-locking
     cider
+    ido-ubiquitous
     smex
     exec-path-from-shell
     projectile
     rainbow-delimiters
+    linum-relative
     ))
+
+;; Get latest versions of all packages
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (dolist (p my-packages)
     (when (not (package-installed-p p))
         (package-install p)))
+
+;; No Backup
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+;; Costumizations
+(add-to-list 'load-path "~/.emacs.d/customizations")
+(load "ui.el")
+(load "themes.el")
