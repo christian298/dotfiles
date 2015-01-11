041596ca6afdaaff53a7beda3d3a102cfd3bda62
Christian Sandvoß <christian.sandvoss@gmail.com>
2015-01-11 20:06:12 +0100

    added emacs web mode

---
+++ .emacs.d/customizations/config-web.el
@@ -0,0 +1,13 @@
+;; JS
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+
+;; Web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
